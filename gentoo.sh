add8d4522b881ef09c0623a835e399cf7e9a3af2
thecatvoid <voidcat@tutanota.com>
2023-05-08 22:01:46 +0530

    update

--- gentoo.sh
+++ gentoo.sh
@@ -37,7 +37,7 @@ setup_build_cmd() {
         cp -af "${HOME}/portage" /etc/
         sed -i "s/^J=.*/J=\"$(nproc --all)\"/" /etc/portage/make.conf
         ln -sf /var/db/repos/gentoo/profiles/default/linux/amd64/17.1/desktop/systemd/ /etc/portage/make.profile
-        FETCHCOMMAND="/usr/bin/wget -t 5 --passive-ftp \${URI} -P \${DISTDIR}" emerge dev-vcs/git net-misc/aria2 app-accessibility/at-spi2-core
+        emerge dev-vcs/git app-accessibility/at-spi2-core
         rm -rf /var/db/repos/* 
         emerge --sync
 }
