ed558f0d4f7829e6153cf538d6f65597e35c985a
thecatvoid <voidcat@tutanota.com>
2023-05-09 12:04:20 +0530

    update

--- gentoo.sh
+++ gentoo.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 set -e
-
 chroot="${HOME}/gentoo"
 trap 'sudo umount -Rf "${chroot}"/* > /dev/null 2>&1 || true' EXIT
 
 rootch() {
-	sudo mount --rbind /dev "${chroot}/dev"
-	sudo mount --make-rslave "${chroot}/dev"
-	sudo mount -t proc /proc "${chroot}/proc"
-	sudo mount --rbind /sys "${chroot}/sys"
-	sudo mount --make-rslave "${chroot}/sys"
-	sudo mount --rbind /tmp "${chroot}/tmp"
-	sudo mount --bind /run "${chroot}/run"
-	sudo cp -L /etc/resolv.conf "${chroot}/etc/"
-	sudo cp -a ./* "${chroot}/root/"
-	sudo chroot "${chroot}" /root/gentoo.sh "$@"
+        sudo mount --rbind /dev "${chroot}/dev"
+        sudo mount --make-rslave "${chroot}/dev"
+        sudo mount -t proc /proc "${chroot}/proc"
+        sudo mount --rbind /sys "${chroot}/sys"
+        sudo mount --make-rslave "${chroot}/sys"
+        sudo mount --rbind /tmp "${chroot}/tmp"
+        sudo mount --bind /run "${chroot}/run"
+        sudo cp -L /etc/resolv.conf "${chroot}/etc/"
+        sudo cp -a ./* "${chroot}/root/"
+        sudo chroot "${chroot}" /root/gentoo.sh "$@"
+}
+
+bashin() {
+        rootch /bin/bash
 }
 
 setup_chroot() {
-        url="https://gentoo.osuosl.org/releases/amd64/autobuilds/current-stage3-amd64-systemd/"
+        url="https://gentoo.osuosl.org/releases/amd64/autobuilds/current-stage3-amd64-desktop-systemd/"
         file="$(curl -s "$url" | grep -Eo 'href=".*"' | awk -F '>' '{print $1}' |
-                sed 's/href=//g' | sed 's/"//g' |
-                grep -Eo "stage3-amd64-systemd-$(date +%Y).*.tar.xz" | uniq)"
+                sed 's/href=//g' -e 's/"//g' | grep -o "stage3-amd64-desktop-systemd-$(date +%Y).*.tar.xz" | uniq)"
+
         curl -sSL "${url}${file}" -o "/var/tmp/${file}"
         mkdir "$chroot"
         sudo tar -C "${chroot}" -xpf "/var/tmp/${file}" --xattrs-include='*.*' --numeric-owner 2>/dev/null
@@ -33,7 +36,7 @@ setup_build_cmd() {
         emerge-webrsync
         cp -af "${HOME}/portage" /etc/
         sed -i "s/^J=.*/J=\"$(nproc --all)\"/" /etc/portage/make.conf
-        ln -sf /var/db/repos/gentoo/profiles/default/linux/amd64/17.1/systemd/ /etc/portage/make.profile
+        ln -sf /var/db/repos/gentoo/profiles/default/linux/amd64/17.1/desktop/systemd /etc/portage/make.profile
         emerge dev-vcs/git app-accessibility/at-spi2-core
         rm -rf /var/db/repos/* 
         emerge --sync
@@ -48,11 +51,13 @@ build_cmd() {
 
 buildpkgs_cmd() {
         rm -rf /var/cache/binpkgs/*
-	curl -sS "https://raw.githubusercontent.com/thecatvoid/gentoo-bin/main/Packages" \
-	-o /var/cache/binpkgs/Packages
-	qlist -I | grep -Ev -- 'acct-user/.*|acct-group/.*|virtual/.*|sys-kernel/.*-sources|.*/.*-bin' |
-	xargs quickpkg --include-config=y
-	fixpackages
+        curl -sS "https://raw.githubusercontent.com/thecatvoid/gentoo-bin/main/Packages" \
+                -o /var/cache/binpkgs/Packages
+
+        qlist -I | grep -Ev -- 'acct-user/.*|acct-group/.*|virtual/.*|sys-kernel/.*-sources|.*/.*-bin' |
+                xargs quickpkg --include-config=y
+
+        fixpackages
         emaint --fix binhost
 }
 
